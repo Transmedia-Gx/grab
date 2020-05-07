3152b08d6f9b14e63034459a330210b680977c78
Taka Iwai <hello@takaiwai.com>
2020-05-07 19:43:08 +0900

    Implement jpg export feature

--- examples/A_MinimumSetup/A_MinimumSetup.pde
+++ examples/A_MinimumSetup/A_MinimumSetup.pde
@@ -2,25 +2,19 @@
 import imageexport.*;
 
 // Activate the Image Export library
-// If you hit the p or j key,
-// a PDF or jpeg image will be exported under exports/ directory
+// Hit p key for exporting PDF, and j for jpg
 ImageExport export = new ImageExport(this);
 
+
+
+// Basic setup() and draw()
 void setup() {
-  // Create a canvas size of 500x500
   size(500, 500);
-  
-  // Make the sketch crisp on RetinaDisplay
   pixelDensity(displayDensity());
 }
 
 void draw() {
-  // Set the background to black
   background(0);
-  
-  // Use white fill color
   fill(255);
-  
-  // Draw a circle at the position of the mouse
   circle(mouseX, mouseY, 50);
 }
--- src/imageexport/ImageExport.java
+++ src/imageexport/ImageExport.java
@@ -1,21 +1,22 @@
 package imageexport;
 
 import processing.core.*;
+import processing.event.KeyEvent;
 
 /**
- * This is a template class and can be used to start a new processing Library.
- * Make sure you rename this class as well as the name of the example package 'template' 
- * to your own Library naming convention.
+ * An easy-to-use pdf/jpeg exporter
  */
-
-public class ImageExport {
+public class ImageExport implements PConstants {
 	
 	// myParent is a reference to the parent sketch
 	PApplet parent;
 	
+	// Version of the library
 	public final static String VERSION = "##library.prettyVersion##";
 	
-
+	// default key for jpg export
+	private final static String JPG_KEY = "j";
+	
 	/**
 	 * a Constructor, usually called in the setup() method in your sketch to
 	 * initialize and start the Library.
@@ -41,6 +42,7 @@ public class ImageExport {
 	private void registerForEvents() {
 		parent.registerMethod("pre", this);
 		parent.registerMethod("post", this);
+		parent.registerMethod("keyEvent", this);
 	}
 	
 	/**
@@ -52,19 +54,56 @@ public class ImageExport {
 		return VERSION;
 	}
 	
-
 	/**
 	 * Start the capturing before the draw() call
 	 */
 	public void pre() {
-		System.out.println("pre()");
 	}
 
 	/**
 	 * Complete the capturing after the draw() call
 	 */
 	public void post() {
-		System.out.println("post()");
+	}
+	
+	/**
+	 * Event handler for key events
+	 */	
+	public void keyEvent(KeyEvent e) {
+		if (e.getAction() == KeyEvent.PRESS) {
+			String lowercaseKey = Character.toString(e.getKey()).toLowerCase();
+			
+			if (lowercaseKey.equals(JPG_KEY)) {
+			    System.out.println("[" + JPG_KEY + "] is typed");
+			    exportJpg();
+			}
+		}		
+	}
+	
+	/**
+	 * Export a jpg file
+	 */
+	private void exportJpg() {
+	    String jpgFilename = getFileNameForExport("jpg");
+	    System.out.println("  --> Saving " + jpgFilename);
+	    parent.save(jpgFilename);
+	    System.out.println("  --> Done saving the jpg!");
+	}
+
+	/**
+	 * return the filename based on the current date and time
+	 * 
+	 * @return String
+	 */
+	private String getFileNameForExport(String extension) {
+		return String.format(
+			"exported/artwork_%02d%02d-%02d%02d%02d.%s",
+			PApplet.month(),
+			PApplet.day(),
+			PApplet.hour(),
+			PApplet.minute(),
+			PApplet.second(),
+			extension
+		);
 	}
 }
-
