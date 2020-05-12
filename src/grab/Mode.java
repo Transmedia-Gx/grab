382cc75c9df4b5257d372b91f96fa05354668248
Taka Iwai <hello@takaiwai.com>
2020-05-13 08:51:44 +0900

    Annotate each case of Mode enum

--- src/grab/Mode.java
+++ src/grab/Mode.java
@@ -4,6 +4,13 @@ package grab;
  * Represents how the user trigger exporting
  */
 public enum Mode {
+	/**
+     * Use keyboard to trigger exporting (p: PDF, j: JPG)
+     */
 	KEYBOARD,
+	
+	/**
+     * Use mouse to trigger exporting (left click: PDF, right click: JPG)
+     */
 	MOUSE
 }
